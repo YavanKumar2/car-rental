a8d630cd5d6bae2205bc391b49f949b0b8031270
yavan <yavankumar2@gmail.com>
2023-12-30 22:48:24 +0530

    first commit

---
+++ src/assets/data/carData.js
@@ -0,0 +1,142 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/nissan-offer.png";
+import img02 from "../all-images/cars-img/offer-toyota.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+// import img04 from "../all-images/cars-img/nissan-offer.png";
+import img04 from "../all-images/cars-img/audiA6.png"
+import img05 from "../all-images/cars-img/hondaClarity (1).png";
+import img06 from "../all-images/cars-img/mercedes-offer.png";
+import img07 from "../all-images/cars-img/volkswagen (1).png";
+import img08 from "../all-images/cars-img/bentleyContinentalGT (1).png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Nissan",
+    rating: 112,
+    carName: "Nissan Maxima",
+    imgUrl: img01,
+    model: "Model-2016",
+    price: 50,
+    fuelType: "Gasoline",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Manual",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "Toyota",
+    rating: 102,
+    carName: "Toyota Camry",
+    imgUrl: img02,
+    model: "Model-2014",
+    price: 50,
+    fuelType: "Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Manual",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "BMW",
+    rating: 132,
+    carName: "BMW M6",
+    imgUrl: img03,
+    model: "Model-2018",
+    price: 265,
+    fuelType: "Diesel",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Audi",
+    rating: 102,
+    carName: "Audi A6",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 300,
+    fuelType: "Diesel",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Honda",
+    rating: 94,
+    carName: "Honda Clarity",
+    imgUrl: img05,
+    model: "Model-2021",
+    price: 45,
+    fuelType: 'Gasoline',
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Manual",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes-Benz",
+    rating: 119,
+    carName: "Mercedes-Benz C-Class",
+    imgUrl: img06,
+    model: "Model-2015",
+    price: 245,
+    fuelType: "Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "transmission",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Volkswagen",
+    rating: 82,
+    carName: "Volkswagen Tiguan",
+    imgUrl: img07,
+    model: "Model-2022",
+    price: 50,
+    fuelType: "Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Manual",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Bentley",
+    rating: 52,
+    carName: "Bentley Continental GT",
+    imgUrl: img08,
+    model: "Model-2023",
+    price: 360,
+    fuelType: "Petrol",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    transmission: "Manual",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
